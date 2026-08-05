f7dae4afc2dee32a0a9b3bf88be13c81076b49d9
Anas Hesham <anassheshamm@gmail.com>
2026-08-05 11:49:57 +0300

    fix: correct api import path

--- frontend/src/features/Admin/pages/patients/PatientsPage.jsx
+++ frontend/src/features/Admin/pages/patients/PatientsPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import api from "../../../services/api";
+import api from "../../../../services/api";
 import PatientsHeader from "../../../components/PageHeader";
 import PatientsTable from "./PatientsTable";
 
